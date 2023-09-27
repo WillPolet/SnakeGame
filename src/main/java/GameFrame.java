3a5020eeac8954c683312b0fb8a09fb25e0225c2
WillPolet <williamplomac@gmail.com>
2023-09-27 11:22:22 +0200

    Snake game, playable. WIP

---
+++ src/main/java/GameFrame.java
@@ -0,0 +1,15 @@
+import javax.swing.*;
+
+public class GameFrame extends JFrame {
+    GameFrame(){
+        GamePanel panel = new GamePanel();
+        this.add(panel);
+        this.setTitle("Snake");
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setResizable(false);
+        this.pack();
+        this.setVisible(true);
+        this.setLocationRelativeTo(null);
+        //this.dispose(); close the instance without closing program, ok it's what I want but not here.
+    }
+}
---
+++ src/main/java/SnakeGame.java
@@ -0,0 +1,5 @@
+public class SnakeGame {
+    public static void main(String[] args) {
+        new GameFrame(); // Same as : GameFrame frame = new GameFrame();
+    }
+}
